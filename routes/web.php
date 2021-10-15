7f3835b2ed6ee01a337bb90a5ae2436639e97984
Araceli <araceli292.nm@gmail.com>
2021-10-15 18:36:01 +0000

    Paso 3

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,29 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('usuarios', function () {
+    return '<h1>Usuarios</h1>';
+});
+
+// Route::get('usuarios/detalles', function () {
+//     return 'Mostrando detalles del usuario: ' . $_GET['id'];
+// });
+
+Route::get('usuarios/nuevo', function () {
+    return 'Creando un nuevo usuario';
+});
+
+Route::get('usuarios/{id}', function ($id) {
+    return 'Mostrando detalles del usuario: ' . $id;
+}) -> where('id', '[0-9]+');
+
+Route::get('saludo/{name}/{nickname?}', function ($name, $nickname = null) {
+    // if ($nickname) {
+    //     return 'Bienvenido ' . $name . '. Tu apodo es ' . $nickname;
+    // } 
+    // return 'Bienvenido ' . $name . '.';
+    return $nickname
+        ? 'Bienvenido ' . $name . '. Tu apodo es ' . $nickname
+        : 'Bienvenido ' . $name . '.';
+});
