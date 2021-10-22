cc536345689e52d05b8328c47b2a790682a8557c
Araceli <araceli292.nm@gmail.com>
2021-10-22 17:01:34 +0000

    Paso 12

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,9 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->truncateTables(['professions']);
+        $this->truncateTables(['professions', 'users']);
 
         $this->call(ProfessionSeeder::class);
+        $this->call(UserSeeder::class);
     }
 
     private function truncateTables(array $tables)
