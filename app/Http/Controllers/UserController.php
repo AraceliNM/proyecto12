2e0fa291dd17f09ef4a2f4098d900c91b6358c25
Araceli <araceli292.nm@gmail.com>
2021-11-02 15:10:41 +0000

    Paso 36

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -65,7 +65,7 @@ class UserController extends Controller
     {
         $data = request()->validate([
             'name' => 'required',
-            'email' => 'required|email|unique:users,email',
+            'email' => 'required|email|unique:users,email,' . $user->id,
             'password' => '',
         ]);
 
