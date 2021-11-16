6ec7257ccf98577e61e857dd4eb7bea0de81d31a
Araceli <araceli292.nm@gmail.com>
2021-11-16 16:05:26 +0000

    Paso 18

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -18,8 +18,21 @@ class ProfileController extends Controller
         ]);
     }
 
-    public function update()
+    public function update(Request $request)
     {
+        $user = User::first(); //$user = auth()->user();
 
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+        ]);
+
+        $user->profile->update([
+            'bio' => $request->bio,
+            'twitter'=> $request->twitter,
+            'profession_id' => $request->profession_id
+        ]);
+
+        return back();
     }
 }
